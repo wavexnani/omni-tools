a257896f0ffa95bb245750e949c600ebd37869aa
wavexnani <mouli7667@gmail.com>
2025-08-13 23:51:39 +0530

    config the resultfooter

--- src/components/result/ResultFooter.tsx
+++ src/components/result/ResultFooter.tsx
@@ -2,20 +2,25 @@ import { Stack } from '@mui/material';
 import Button from '@mui/material/Button';
 import DownloadIcon from '@mui/icons-material/Download';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import ForwardIcon from '@mui/icons-material/Forward';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function ResultFooter({
   handleDownload,
   handleCopy,
+  handleForward,
   disabled,
   hideCopy,
-  downloadLabel
+  downloadLabel,
+  hideForward
 }: {
   handleDownload: () => void;
   handleCopy?: () => void;
+  handleForward?: () => void;
   disabled?: boolean;
   hideCopy?: boolean;
+  hideForward?: boolean;
   downloadLabel?: string;
 }) {
   const { t } = useTranslation();
@@ -35,6 +40,15 @@ export default function ResultFooter({
           startIcon={<ContentPasteIcon />}
         >
           {t('resultFooter.copy')}
+        </Button>
+      )}
+      {!hideForward && (
+        <Button
+          disabled={disabled}
+          onClick={handleForward}
+          startIcon={<ForwardIcon />}
+        >
+          {t('resultFooter.forward')}
         </Button>
       )}
     </Stack>
